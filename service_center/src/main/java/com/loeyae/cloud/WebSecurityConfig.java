827411ff1bcdd68ec7a76d92bb48f508ed2763d5
ZhangYi <loeyae@gmail.com>
2020-04-23 23:23:13 +0800

    更改认证设置

---
+++ service_center/src/main/java/com/loeyae/cloud/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.loeyae.cloud;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * WebSecurityConfig .
+ *
+ * @date: 2020-04-23
+ * @version: 1.0
+ * @author: zhangyi07@beyondsoft.com
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
+                .anyRequest()
+                .authenticated().and().httpBasic();
+    }
+}
